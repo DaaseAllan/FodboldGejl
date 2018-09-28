f00d62d3ac3047b1f994693fa25d7b12c58d38b8
DaaseAllan <laurits.mumberg@gmail.com>
2018-09-28 14:16:38 +0200

    movement added

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -4,28 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    Rigidbody2D body;
-    float horizontal;
-    float vertical;
+    public enum PlayerNumber
+    {
+        P1,
+        P2
+    };
+
+    public PlayerNumber playerNumber;
+
+    public GameObject rightPlayers;
+    public GameObject leftPlayers;
+
+    Rigidbody2D rightRB;
+    Rigidbody2D leftRB;
+
+    float horizontalR;
+    float verticalR;
+    float verticalL;
+    float horizontalL;
+
     float moveLimiter = 0.7f;
     public float runSpeed = 20;
 
+
+
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        rightRB = rightPlayers.GetComponent<Rigidbody2D>();
+        leftRB = leftPlayers.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        horizontalR = Input.GetAxisRaw(playerNumber + "HorizontalR");
+        verticalR = Input.GetAxisRaw(playerNumber + "VerticalR");
+
+        horizontalL = Input.GetAxisRaw(playerNumber + "HorizontalL");
+        verticalL = Input.GetAxisRaw(playerNumber + "VerticalL");
+
+        Debug.Log(horizontalL);
     }
 
     void FixedUpdate()
     {
-        if (horizontal != 0 && vertical != 0)
-            body.velocity = new Vector2((horizontal * runSpeed) * moveLimiter, (vertical * runSpeed) * moveLimiter);
+        //Højre
+        if (horizontalR != 0 && verticalR != 0)
+            rightRB.velocity = new Vector2((horizontalR * runSpeed) * moveLimiter, (verticalR * runSpeed) * moveLimiter);
         else
-            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+            rightRB.velocity = new Vector2(horizontalR * runSpeed, verticalR * runSpeed);
+
+        //Venstre
+        if (horizontalL != 0 && verticalL != 0)
+            leftRB.velocity = new Vector2((horizontalL * runSpeed) * moveLimiter, (verticalL * runSpeed) * moveLimiter);
+        else
+            leftRB.velocity = new Vector2(horizontalL * runSpeed, verticalL * runSpeed);
     }
 }
