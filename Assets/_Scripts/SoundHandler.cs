3517724d91ff09262f4db75975fa8ff0cbf13f4f
Steven <thedankpure@gmail.com>
2018-10-12 03:19:06 +0900

    audio

---
+++ Assets/_Scripts/SoundHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour {
+
+    AudioSource audioSource;
+    public static SoundHandler soundHandler;
+    public AudioClip[] sounds;
+
+	void Start () {
+        soundHandler = GetComponent<SoundHandler>();
+        audioSource = GetComponent<AudioSource>();
+	}
+
+	public void playSound(int s)
+    {
+        audioSource.PlayOneShot(sounds[s]);
+    }
+}
